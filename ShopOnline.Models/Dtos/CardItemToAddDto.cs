1ba12acbbc095dd864ce6e604969bc591a785d6b
emilio barrera <barrera_emilio@yahoo.es>
2023-01-06 02:22:49 +0000

    classes  Dtos

---
+++ ShopOnline.Models/Dtos/CardItemToAddDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ShopOnline.Models.Dtos
+{
+    public class CardItemToAddDto
+    {
+        public int CardId { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Qty { get; set; }
+    }
+}
---
+++ ShopOnline.Models/Dtos/CartItemQtyUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ShopOnline.Models.Dtos
+{
+    public class CartItemQtyUpdateDto
+    {
+        public int CardItemId { get; set; }
+
+        public int Qty { get; set; }
+    }
+}
